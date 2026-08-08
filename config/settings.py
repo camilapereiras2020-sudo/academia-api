342950d52cfaa06f027432897872aa1830eb8485
camila.pereiras2020-sudo <camila.pereiras2020@gmail.com>
2026-08-08 10:41:09 +0200

    Add whatsapp_reply and dashboard to LOCAL_APPS

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,8 @@ LOCAL_APPS = [
     "modules.empresas",
     "modules.clases",
     "modules.tarifas",
+    "modules.whatsapp_reply",
+    "modules.dashboard",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
